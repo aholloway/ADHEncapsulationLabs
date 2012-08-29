e15652af058f493326dc7e99b3596d65f2740b72
aholloway <aholloway@L11713038597.wctc.msft>
2012-08-29 18:11:29 -0500

    Added "Goodbye" and Second Class.

--- src/adhencapsulationlabs/ADHEncapsulationLabs.java
+++ src/adhencapsulationlabs/ADHEncapsulationLabs.java
@@ -15,5 +15,6 @@ public class ADHEncapsulationLabs {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Goodbye");
     }
 }
